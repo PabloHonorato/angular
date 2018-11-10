5300753b30a7e190ced0a487baabcb700d4397d3
PabloHonorato <pblhonorato@gmail.com>
2018-11-10 12:10:55 -0200

    aula angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,27 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'pablodevs';
+ tasks : task[] =[
+    {taskName: "arrumar cama", done: true},
+    {taskName: "fazer comida", done: true},
+    {taskName: "tomar banho", done: false},
+    {taskName: "arrumar a casa", done: false},
+ ];
+ add(taskName, done){
+  let task: task = {taskName: taskName, done: done};
+  this.tasks.push(task);
+   console.log(task);
+ }
+ del(index){
+   this.tasks.splice(index , 1);
+ }
+
+}
+
+class task {
+  taskName: string;
+  done:boolean;
+
 }
